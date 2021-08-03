a6702aa8805ee5c41bab4590d804e0bbd4626daa
AadyaaJhunjhunwala <aadyaarox999@gmail.com>
2021-08-03 18:58:17 +0530

    Added functions to improve the code

--- Pig.js
+++ Pig.js
@@ -2,10 +2,24 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibility=255
   }
 display()
 {
-  console.log(this.body.speed)
+ // console.log(this.body.speed)
+ 
+if(this.body.speed<3){
+super.display();}
+else{
+  World.remove(world,this.body)
 
-super.display();
+  push()
+  tint(225,this.visibility);
+  this.visibility= this.visibility-5
+  image(this.image, this.body.position.x, this.body.position.y, 50, 50)
+  pop()
+}
+
+
+
 }};
